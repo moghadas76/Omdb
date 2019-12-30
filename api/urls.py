f2622d13b8e5616bb3b80b768ca384be83a11c1a
moghadas76 <s.m.moghadas2012@gmail.com>
2019-12-30 04:27:13 +0330

    f release

--- api/urls.py
+++ api/urls.py
@@ -4,5 +4,18 @@ from api import views
 
 urlpatterns = [
     path('search/',views.SearchByNameApi.as_view()),
-    path("movie_search/", views.SearchByImdbIdApi.as_view(), name="imdb")
+    path("movie_search/", views.SearchByImdbIdApi.as_view(), name="imdb"),
+    # path("upcomings/", views.UpcomingApi.as_view(), name="imdb"),
+    path("movie_tags/", views.GetTags.as_view(), name="tags"),
+    path("movie_search_tags/<int:g_id>/<int:page>/", views.SearchByTagApi.as_view(), name="find_tag"),
+    path("ip/", views.InfoByIp.as_view(), name="ip"),
+    path("address/", views.ObtainAddress.as_view(), name="address"),
+    path("url_shorten/", views.UrlShorten.as_view(), name="short_url"),
+    path("country_info/<str:country>/", views.FindCountryInfo.as_view(), name="country"),
+    path("weather/", views.Weather.as_view(), name="weather"),
+    path("tts/<str:text>", views.TextToSpeech.as_view(), name="tts"),
+    path("qr/<str:text>", views.QrGenerator.as_view(), name="qr"),
+    # path("movie_search/", views.SearchByImdbIdApi.as_view(), name="imdb"),
+    # path("movie_search/", views.SearchByImdbIdApi.as_view(), name="imdb"),
+
 ]
--- api/views.py
+++ api/views.py
@@ -48,3 +48,105 @@ class SearchByImdbIdApi(APIView):
             resp = json.loads(response.content)
             return Response(resp)
 
+
+
+class GetTags(APIView):
+
+    def get(self,request):
+        api_endpint = 'http://moviesapi.ir/api/v1/genres'
+        response = requests.get(api_endpint)
+        resp = json.loads(response.content)
+        return Response(resp)
+
+
+class SearchByTagApi(APIView):
+
+    def get(self,request,g_id,page):
+        # pg = request.GET.get('page','')
+        api_endpint = 'http://moviesapi.ir/api/v1/genres/{0}/movies?page={1}'.format(g_id,page)
+        response = requests.get(api_endpint)
+        resp = json.loads(response.content)
+        return Response(resp)
+
+    
+
+class InfoByIp(APIView):
+
+    def get(self,request):
+        api_endpint = 'https://api.ipgeolocation.io/ipgeo?apiKey=3099885ac5d746899364c64640b53a6c'
+        response = requests.get(api_endpint)
+        resp = json.loads(response.content)
+        return Response(resp)
+
+def build_url(page):
+    return "http://127.0.0.1:8000" + page
+
+class ObtainAddress(APIView):
+
+    def get(self,request):
+        api_endpint = build_url('/api/v1/ip')
+        response = requests.get(api_endpint)
+        resp = json.loads(response.content)
+        lat = resp['latitude']
+        lang = resp['longitude']
+        api_endpint = 'https://api.neshan.org/v2/reverse?lat={0}&lng={1}'.format(lat,lang)
+        headers = {'Api-Key':'service.YDy990oXoofQgcTGLj5bwiIl8m36DqxC46Md7bav'}
+        response = requests.get(api_endpint,headers=headers)
+        resp = json.loads(response.content)
+        return Response(resp)
+
+
+
+class FindCountryInfo(APIView):
+
+    def get(self,request,country):
+        api_endpint = 'https://restcountries.eu/rest/v2/name/{0}'.format(country)
+        response = requests.get(api_endpint)
+        resp = json.loads(response.content)
+        return Response(resp)
+
+
+class UrlShorten(APIView):
+
+    def get(self,request):
+        api_endpint = 'http://api.ipstack.com/check?access_key=32b2d4ff44a25e933213340faf94e3ea&format=1'
+        response = requests.get(api_url)
+        resp = json.loads(response.content)
+        return Response(resp)
+
+
+
+
+class Weather(APIView):
+
+    def get(self,request):
+        api_endpint =build_url('/api/v1/ip')
+        response = requests.get(api_endpint)
+        resp = json.loads(response.content)
+        lat = resp['latitude']
+        lang = resp['longitude']
+        api_endpint = 'http://api.openweathermap.org/data/2.5/weather?lat={0}&lon={1}&APPID={2}'.format(lat,lang,'4b492e893c215c7ef16db91abd94effc')
+        response = requests.get(api_endpint)
+        resp = json.loads(response.content)
+        return Response(resp)
+
+
+# Move to frontend
+class TextToSpeech(APIView):
+
+    def get(self,request,text):
+        api_endpint = 'http://api.voicerss.org/?key=0781320d756a454181c8047b5dbfaf7f&hl=en-us&src={0}'.format(text)
+        response = requests.get(api_endpint)
+        resp = json.loads(response.content)
+        return Response(resp)
+
+
+# Move to frontend
+class QrGenerator(APIView):
+
+    def get(self,request,text):
+        api_endpint = 'https://api.qrserver.com/v1/create-qr-code/?size=150x150&data={0}'.format(text)
+        response = requests.get(api_endpint)
+        resp = json.loads(response.content)
+        return Response(resp)
+
